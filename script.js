c109675b889bf51b0ea12953f6c5e5b93fd5f451
nibinotr <otrnibin@gmail.com>
2024-08-13 02:58:51 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function changelogo() {
+    let logo = document.getElementById("logo");
+    logo.src = "./images/JavaScript-Logo.jpg"; 
+}
+
+function reset() {
+    let logo = document.getElementById("logo");
+    logo.src = "./images/html-5.avif"; 
+}
